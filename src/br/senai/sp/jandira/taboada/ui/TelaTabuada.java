443ed87a256b748e33be0c5269d6c91eea149d88
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2025-11-10 15:50:18 -0300

    criando estrutura básica da tela

--- src/br/senai/sp/jandira/taboada/ui/TelaTabuada.java
+++ src/br/senai/sp/jandira/taboada/ui/TelaTabuada.java
@@ -2,6 +2,8 @@ package br.senai.sp.jandira.taboada.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -20,12 +22,39 @@ public class TelaTabuada extends Application {
 
         // criando o root - componente principal
         VBox root = new VBox();
+        root.setStyle("-fx-background-color: blue;");
 
         // criando a cena e colocando o root nela
         Scene scene = new Scene(root);
 
         // colocando a cena no palco
         stage.setScene(scene);
+
+        // criando header
+        VBox header = new VBox();
+        header.setPrefHeight(100);
+        header.setStyle("-fx-background-color: green;");
+
+        // criando grid de formulário
+        GridPane gridformulario = new GridPane();
+        gridformulario.setPrefHeight(100);
+        gridformulario.setStyle("-fx-background-color: yellow;");
+
+        // criando a caixa de botões
+        HBox boxBotoes = new HBox();
+        boxBotoes.setPrefHeight(100);
+        boxBotoes.setStyle("-fx-background-color: red;");
+
+        // criando a caixa de resultados
+        VBox boxResultados = new VBox();
+        boxResultados.setPrefHeight(100);
+        boxResultados.setStyle("-fx-background-color: purple;");
+
+        // adicionar componentes no root
+        root.getChildren().add(header);
+        root.getChildren().add(gridformulario);
+        root.getChildren().add(boxBotoes);
+        root.getChildren().add(boxResultados);
 
         // stage show é pra mostrar a tela
         stage.show();
